1f2af490ae1089d94a5d0f13154fea47afb7c7a8
admin <admin@admindeMacBook-Pro.local>
2015-05-19 22:54:02 +0800

    Initial Commit

---
+++ DiZiES/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiZiES
+//
+//  Created by admin on 15/5/19.
+//  Copyright (c) 2015年 DiZiCompanyLimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
